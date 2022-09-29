84b124bf9589bf96c42afa4cdeba515e46e087c5
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-09-29 12:08:45 +0600

    user height weight age details

--- src/Components/Useractivity/Useractivity.js
+++ src/Components/Useractivity/Useractivity.js
@@ -14,6 +14,21 @@ const Useractivity = () => {
           <p className='location'>Lakshmipur, Bangladesh</p>
         </div>
       </div>
+
+      <div className="user-physical-details">
+        <div className="weight">
+          <h2>60<small>kg</small></h2>
+          <p>Weight</p>
+        </div>
+        <div className="height">
+          <h2>5.6<small>in</small></h2>
+          <p>Height</p>
+        </div>
+        <div className="age">
+          <h2>22<small>yrs</small></h2>
+          <p>Age</p>
+        </div>
+      </div>
     </div>
   );
 };
