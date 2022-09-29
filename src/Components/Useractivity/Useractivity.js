3fb8ee6805e1f48da72413ed30fe461a0836959f
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-09-29 11:54:54 +0600

    user details working

--- src/Components/Useractivity/Useractivity.js
+++ src/Components/Useractivity/Useractivity.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Useractivity.css';
+import User from '../../images/user.jpg';
 
 const Useractivity = () => {
   return (
     <div className='user-activity'>
-      <h2>User Activity Section</h2>
+      <div className='user-container'>
+        <div>
+          <img className='user-img' src={User} alt="" />
+        </div>
+        <div className='user-details'>
+          <h3 className='user-name'>Abdulla Al Anas Saimon</h3>
+          <p className='location'>Lakshmipur, Bangladesh</p>
+        </div>
+      </div>
     </div>
   );
 };
